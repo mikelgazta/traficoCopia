da9a999a61169a99e89ec0a41bf627195a012350
Mikel <mikelgaztargmail.com>
2024-01-29 07:49:19 +0100

    Pre-gripe

--- Android/app/src/main/java/com/example/traficoandroid/MapFragment.java
+++ Android/app/src/main/java/com/example/traficoandroid/MapFragment.java
@@ -25,7 +25,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.activity_map, container, false);
 
         mapView = view.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
