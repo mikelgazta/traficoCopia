0fbb2496fdaa8a69103df8f11fc1fa45a89e94fc
mikelgazta <mikelgazta@gmail.com>
2024-02-02 12:14:07 +0100

    Api Funcionando correctamente con Middleware de autenticacion

--- ApiLaravel/app/Http/Middleware/Authenticate.php
+++ ApiLaravel/app/Http/Middleware/Authenticate.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Usuario; // Ajusta la ruta del modelo según sea necesario
+
 
 class Authenticate
 {
@@ -13,16 +15,22 @@ class Authenticate
         if (!$request->header('Authorization')) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
-
+    
         // Obtener el token de autorización de la solicitud
         $token = $request->header('Authorization');
-
-        // Intentar autenticar al usuario utilizando el token
-        if (!Auth::onceUsingId($token)) {
+    
+        // Buscar el usuario por el token en la base de datos
+        $usuario = Usuario::where('TOKEN', $token)->first();
+    
+        // Verificar si se encontró un usuario con el token dado
+        if (!$usuario) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
-
-        // Si la autenticación es exitosa, permitir que la solicitud continúe hacia el controlador
+    
+        // Autenticar al usuario
+        Auth::login($usuario);
+    
+        // Permitir que la solicitud continúe hacia el controlador
         return $next($request);
     }
 }
